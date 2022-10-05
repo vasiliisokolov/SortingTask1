ecf4738d27112173f457ee72623f696e80331ea3
VasiliiSokolov <vasilysokolov@mail.ru>
2022-10-05 21:19:33 +0300

    Добавьте файлы проекта.

---
+++ SortingTask!.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+int addition(int, int, std::vector<int>&);
+
+int main()
+{
+    std::vector<int> a = { -2,1,-3,4,-1,2,1,-5,4 };
+    int sum = 0;
+    int maxSum = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = i+1; j < a.size(); j++)
+        {
+            sum = addition(i, j, a);
+        }
+        std::cout << sum;
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+        }
+    }
+    std::cout << "Result: " << maxSum;
+}
+
+int addition(int i, int j, std::vector<int>& a)
+{
+    int sum = 0;
+    for (; i <= j; i++)
+    {
+        sum += a[i];
+    }
+    return sum;
+}
